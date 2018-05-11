53d7ad5ef1914d26a6181a59d76ca6770312390d
your_name <your_email_address@qq.com>
2018-05-11 16:10:26 +0800

    wqwdsa

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 app_name = 'blog'
 urlpatterns = [
-     path('', views.home_page, name='home_page'),
+     path('', views.home_page, name = 'home_page'),
 ]
